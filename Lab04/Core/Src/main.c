73f3c8b2ac5e79c4c1b4baacd73da0ac8322fce3
0i0iSaad <sa10110@st.habib.edu.pk>
2026-02-06 12:51:11 +0500

    Task 4 done

--- Lab04/Core/Src/main.c
+++ Lab04/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stdio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -47,6 +48,8 @@ SPI_HandleTypeDef hspi1;
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 
+UART_HandleTypeDef huart2;
+
 PCD_HandleTypeDef hpcd_USB_FS;
 
 /* USER CODE BEGIN PV */
@@ -61,12 +64,16 @@ static void MX_SPI1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_USB_PCD_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+char buffer[50];
+uint32_t last_capture = 0, period = 0;
+float frequency = 0.0f;
 
 //Task 1
 //   void delay_ms(uint32_t ms)
@@ -116,13 +123,13 @@ int main(void)
   MX_TIM2_Init();
   MX_USB_PCD_Init();
   MX_TIM3_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
   // __HAL_TIM_SET_COUNTER(&htim2, 0);
   // HAL_TIM_Base_Start(&htim2);
   // HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
@@ -130,7 +137,9 @@ int main(void)
   //Task 2
   // HAL_TIM_Base_Start_IT(&htim2);
 
-  HAL_TIM_IC_Start_IT (&htim3 , TIM_CHANNEL_3);
+  //Task 4
+  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_3);
+
   while (1)
   {
     //Task 1
@@ -180,7 +189,9 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_USART2
+                              |RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
   PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
@@ -278,6 +289,59 @@ static void MX_SPI1_Init(void)
 
 }
 
+//task 2
+  // void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef *htim) {
+  //   if (htim -> Instance == TIM2) {
+  //     HAL_GPIO_TogglePin (LD3_GPIO_Port , LD3_Pin);
+  //   } 
+  // }
+
+  //Task 3
+  // uint32_t countA = 0;
+  // uint32_t countB = 0;
+  // uint32_t countC = 0;
+  // void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef *htim) {
+  //   if (htim -> Instance == TIM2) {
+  //     countA++;
+  //     countB++;
+  //     countC++;
+
+  //     if(countA >= 500u){
+  //       HAL_GPIO_TogglePin (LD3_GPIO_Port , LD3_Pin);
+  //       countA = 0;
+  //     }
+
+  //     if(countB >= 200u){
+  //       HAL_GPIO_TogglePin (LD4_GPIO_Port , LD4_Pin);
+  //       countB = 0;
+  //     }
+
+  //     if(countC >= 100u){
+  //       HAL_GPIO_TogglePin (LD5_GPIO_Port , LD5_Pin);
+  //       countC = 0;
+  //     }
+  //   } 
+  // }
+
+  //Task 4
+  void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM3 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3) {
+        uint32_t current_capture = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_3);
+        if (current_capture >= last_capture) {
+            period = current_capture - last_capture;
+        } else {
+            period = (htim->Init.Period - last_capture) + current_capture + 1;
+        }
+        last_capture = current_capture;
+
+        if (period != 0) {
+            frequency = 1000000.0f / (float)period; // frequency in Hz
+            int len = sprintf(buffer, "Frequency: %.2f Hz\r\n", frequency);
+            HAL_UART_Transmit(&huart2, (uint8_t*)buffer, len, 10);
+        }
+    }
+  }
+
 /**
   * @brief TIM2 Initialization Function
   * @param None
@@ -297,7 +361,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 47999;
+  htim2.Init.Prescaler = 47;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 9999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -323,61 +387,6 @@ static void MX_TIM2_Init(void)
 
 }
 
-//task 2
-  // void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef *htim) {
-  //   if (htim -> Instance == TIM2) {
-  //     HAL_GPIO_TogglePin (LD3_GPIO_Port , LD3_Pin);
-  //   } 
-  // }
-
-  //Task 3
-  // void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef *htim) {
-  //   if (htim -> Instance == TIM2) {
-  //     countA++;
-  //     countB++;
-  //     countC++;
-
-  //     if(countA >= 500u){
-  //       HAL_GPIO_TogglePin (LD3_GPIO_Port , LD3_Pin);
-  //       countA = 0;
-  //     }
-
-  //     if(countB >= 200u){
-  //       HAL_GPIO_TogglePin (LD4_GPIO_Port , LD4_Pin);
-  //       countB = 0;
-  //     }
-
-  //     if(countC >= 100u){
-  //       HAL_GPIO_TogglePin (LD5_GPIO_Port , LD5_Pin);
-  //       countC = 0;
-  //     }
-  //   } 
-  // }
-
-  uint32_t last_capture = 0, period = 0;
-  float frequency = 0.0f;
-
-  void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM3 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_3) {
-        uint32_t current_capture = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_3);
-        
-        if (current_capture >= last_capture) {
-            period = current_capture - last_capture;
-        } else {
-            // Timer overflow occurred
-            period = (htim->Init.Period - last_capture) + current_capture + 1;
-        }
-        
-        last_capture = current_capture;
-        
-        // Calculate frequency (assuming timer clock tick is 1 us)
-        if (period != 0) {
-            frequency = 1000000.0f / (float)period;
-        }
-    } 
-  } 
-
-
 /**
   * @brief TIM3 Initialization Function
   * @param None
@@ -397,7 +406,7 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
+  htim3.Init.Prescaler = 47;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 65535;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -423,6 +432,41 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
 
@@ -512,6 +556,7 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
 
 /* USER CODE END 4 */
 
--- Lab04/Core/Src/stm32f3xx_hal_msp.c
+++ Lab04/Core/Src/stm32f3xx_hal_msp.c
@@ -258,16 +258,16 @@ void HAL_TIM_IC_MspInit(TIM_HandleTypeDef* htim_ic)
     /* Peripheral clock enable */
     __HAL_RCC_TIM3_CLK_ENABLE();
 
-    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
     /**TIM3 GPIO Configuration
-    PB0     ------> TIM3_CH3
+    PC8     ------> TIM3_CH3
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     /* TIM3 interrupt Init */
     HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
@@ -322,15 +322,81 @@ void HAL_TIM_IC_MspDeInit(TIM_HandleTypeDef* htim_ic)
     __HAL_RCC_TIM3_CLK_DISABLE();
 
     /**TIM3 GPIO Configuration
-    PB0     ------> TIM3_CH3
+    PC8     ------> TIM3_CH3
     */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0);
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_8);
 
     /* TIM3 interrupt DeInit */
     HAL_NVIC_DisableIRQ(TIM3_IRQn);
     /* USER CODE BEGIN TIM3_MspDeInit 1 */
 
     /* USER CODE END TIM3_MspDeInit 1 */
+  }
+
+}
+
+/**
+  * @brief UART MSP Initialization
+  * This function configures the hardware resources used in this example
+  * @param huart: UART handle pointer
+  * @retval None
+  */
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(huart->Instance==USART2)
+  {
+    /* USER CODE BEGIN USART2_MspInit 0 */
+
+    /* USER CODE END USART2_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* USER CODE BEGIN USART2_MspInit 1 */
+
+    /* USER CODE END USART2_MspInit 1 */
+
+  }
+
+}
+
+/**
+  * @brief UART MSP De-Initialization
+  * This function freeze the hardware resources used in this example
+  * @param huart: UART handle pointer
+  * @retval None
+  */
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+  if(huart->Instance==USART2)
+  {
+    /* USER CODE BEGIN USART2_MspDeInit 0 */
+
+    /* USER CODE END USART2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
+
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
+
+    /* USER CODE BEGIN USART2_MspDeInit 1 */
+
+    /* USER CODE END USART2_MspDeInit 1 */
   }
 
 }
